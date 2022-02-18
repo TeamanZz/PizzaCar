554eb10b2e33e462c0845cf5db14331e0b34a560
Alexey <a.yasinko@mail.ru>
2022-02-18 18:25:51 +0300

    Turning by joystick

--- Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 
-namespace KartGame.KartSystems {
-
+namespace KartGame.KartSystems
+{
     public class KeyboardInput : BaseInput
     {
         public string TurnInputName = "Horizontal";
         public string AccelerateButtonName = "Accelerate";
         public string BrakeButtonName = "Brake";
-
-        public override InputData GenerateInput() {
+        public FloatingJoystick joystick;
+        public override InputData GenerateInput()
+        {
             return new InputData
             {
                 Accelerate = Input.GetButton(AccelerateButtonName),
                 Brake = Input.GetButton(BrakeButtonName),
-                TurnInput = Input.GetAxis("Horizontal")
+                TurnInput = joystick.Horizontal
             };
         }
     }
